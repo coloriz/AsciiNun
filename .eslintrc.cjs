b88b326ed7dd79d4dd05770c024f7e0c8f429417
Jimmy <nunu3041@gmail.com>
2023-03-05 21:40:48 +0900

    Initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/extensions': ['.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      'typescript': {
+        'alwaysTryTypes': true
+      }
+    }
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        'alphabetize': {
+          'order': 'asc'
+        }
+      }
+    ]
+  },
+  ignorePatterns: ['dist/'],
+};
